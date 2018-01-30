3d891c9c03b63dcf613d34c575fbcbaf67ed286d
Unknown <mymintzata@hotmail.co.th>
2018-01-30 12:04:30 +0700

    ทำข้อที่10.3 เสร็จ

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,4 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 int main() {
         /* code */
+        int cmd;
+        string name;
+        string line;
+        ofstream writeFile("std.txt", fstream::app);
+        ifstream readFile ("std.txt");
+
+        do {
+            cout << "Menu" << endl;
+            cout << "1. add student." << endl;
+            cout << "2. list student." << endl;
+            cout << "3. quit." << endl;
+            cout << "Please enter menu : ";
+            cin >> cmd;
+
+            if (cmd == 1){
+                cout << "Enter Std name : ";
+                cin >> name;
+
+                if (writeFile.is_open()){
+                    writeFile << name << endl;
+                    writeFile.close();
+                }else {
+                cout << "Unable to open File";}
+        } else if(cmd == 2) {
+            readFile.open("std.txt");
+            if (readFile.is_open()){
+                while (getline(readFile, line))
+                {
+                    cout << line << endl;
+                }
+                readFile.close();
+            }
+        } else {
+            break;
+        }
+    } while(1);
+
         return 0;
 }
